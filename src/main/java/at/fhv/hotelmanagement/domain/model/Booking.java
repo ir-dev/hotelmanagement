fc5a3efb29a371f5717d1736729c3fea8325150a
ivo <uKATnoqJ=+fk!Fw}BBtd>
2021-11-15 23:12:18 +0100

    remove unnecessary import statement

--- src/main/java/at/fhv/hotelmanagement/domain/model/Booking.java
+++ src/main/java/at/fhv/hotelmanagement/domain/model/Booking.java
@@ -4,7 +4,6 @@ import at.fhv.hotelmanagement.domain.model.enums.BookingStatus;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 import java.util.Map;
 
 public class Booking {
