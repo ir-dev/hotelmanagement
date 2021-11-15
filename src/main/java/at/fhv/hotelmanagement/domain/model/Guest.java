1a9750d3c5e4fbeb17ccbdb32d28e35131f0fcb1
ivo <uKATnoqJ=+fk!Fw}BBtd>
2021-11-15 22:08:08 +0100

    add/remove comments

--- src/main/java/at/fhv/hotelmanagement/domain/model/Guest.java
+++ src/main/java/at/fhv/hotelmanagement/domain/model/Guest.java
@@ -1,27 +1,21 @@
 package at.fhv.hotelmanagement.domain.model;
 
-import at.fhv.hotelmanagement.domain.model.enums.Country;
 import at.fhv.hotelmanagement.domain.model.enums.Salutation;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public class Guest {
+    // generated hibernate id
     private Long id;
     private GuestId guestId;
-    // * Organization Details
     private Optional<Organization> organization;
-    // * Billing Address
     private Salutation salutation;
     private String firstName;
     private String lastName;
     private LocalDate birthday;
     private Address address;
     private String specialNotes;
-
-//    private Country nationality;
-//    private String email;
 
     public Guest(GuestId guestId, Optional<Organization> organization, String salutation, String firstName, String lastName, LocalDate birthday, Address address, String specialNotes) {
         this.guestId = guestId;
