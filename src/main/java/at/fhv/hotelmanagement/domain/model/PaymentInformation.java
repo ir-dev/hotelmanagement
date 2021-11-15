bee490d1e73135d90f26b91eaa89bfad03699cf8
ivo <uKATnoqJ=+fk!Fw}BBtd>
2021-11-15 23:07:40 +0100

    add getters, comments

--- src/main/java/at/fhv/hotelmanagement/domain/model/PaymentInformation.java
+++ src/main/java/at/fhv/hotelmanagement/domain/model/PaymentInformation.java
@@ -3,22 +3,42 @@ package at.fhv.hotelmanagement.domain.model;
 import at.fhv.hotelmanagement.domain.model.enums.PaymentType;
 
 public class PaymentInformation {
-    // Credit Card Details (used for advance payment)
+    // credit card details (used for advance payment)
     private String cardHolderName;
     private String cardNumber;
     private String cardValidThru;
     private String cardCvc;
-    // Payment Type (used for billing)
+    // payment type (used for billing)
     private PaymentType paymentType;
 
-    private PaymentInformation() {
+    // required for hibernate
+    private PaymentInformation() {}
 
-    }
     public PaymentInformation(String cardHolderName, String cardNumber, String cardValidThru, String cardCvc, String paymentType) {
         this.cardHolderName = cardHolderName;
         this.cardNumber = cardNumber;
         this.cardValidThru = cardValidThru;
         this.cardCvc = cardCvc;
         this.paymentType = PaymentType.valueOf(paymentType);
+    }
+
+    public String getCardHolderName() {
+        return this.cardHolderName;
+    }
+
+    public String getCardNumber() {
+        return this.cardNumber;
+    }
+
+    public String getCardValidThru() {
+        return this.cardValidThru;
+    }
+
+    public String getCardCvc() {
+        return this.cardCvc;
+    }
+
+    public PaymentType getPaymentType() {
+        return this.paymentType;
     }
 }
