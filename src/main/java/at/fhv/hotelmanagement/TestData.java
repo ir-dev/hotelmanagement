d3dd26b56c9029d89d31fed350a19545c5d90a39
Whosayin20 <hueseyin.arziman@gmail.com>
2021-11-26 15:35:11 +0100

    Adjusted Application Layer Tests for Factories: allBookings, bookingByBookingNo, bookingDetailsByBookingNo

--- src/main/java/at/fhv/hotelmanagement/TestData.java
+++ src/main/java/at/fhv/hotelmanagement/TestData.java
@@ -8,9 +8,12 @@ import at.fhv.hotelmanagement.domain.model.enums.Salutation;
 import at.fhv.hotelmanagement.domain.repositories.BookingRepository;
 import at.fhv.hotelmanagement.domain.repositories.CategoryRepository;
 import at.fhv.hotelmanagement.domain.repositories.GuestRepository;
+import at.fhv.hotelmanagement.domain.repositories.StayRepository;
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +23,7 @@ import java.util.*;
 
 @Component
 @Transactional
+@Profile("!test")
 public class TestData implements ApplicationRunner {
 
     @Autowired
@@ -49,8 +53,8 @@ public class TestData implements ApplicationRunner {
         c2.createRoom(new Room(new RoomNumber("223"), RoomState.AVAILABLE));
         Room room224 = new Room(new RoomNumber("224"), RoomState.AVAILABLE);
         c2.createRoom(room224);
-        room224.occupied(LocalDate.of(2021,11,19), LocalDate.of(2021,11,22));
-        room224.occupied(LocalDate.of(2021,11,23), LocalDate.of(2021,11,25));
+        room224.occupied(LocalDate.of(2021,11,19), LocalDate.of(2021,11,22), null);
+        room224.occupied(LocalDate.of(2021,11,23), LocalDate.of(2021,11,25), null);
 
         Organization orgaEmpty = null;
         Organization orga1 = new Organization("FHV", "PROMOCODE-XMAS2021");
@@ -74,5 +78,12 @@ public class TestData implements ApplicationRunner {
                 LocalDate.of(2021,12,24), LocalTime.of(11,30), 1, categoryRooms2, g2.getGuestId(), paymentInformation2);
         this.bookingRepository.store(bk1);
         this.bookingRepository.store(bk2);
+
+
+
+//        Stay stay = new Stay(this.stayRepository.nextIdentity(), this.bookingRepository.nextIdentity(), LocalDate.of(2021,11,24),
+//                LocalDate.of(2021,11,26), LocalTime.of(12,30), 4, categoryRooms1, g1.getGuestId(), paymentInformation1);
+//        stayRepository.store(stay);
+
     }
 }
--- src/test/java/at/fhv/hotelmanagement/application/impl/BookingServiceImplTest.java
+++ src/test/java/at/fhv/hotelmanagement/application/impl/BookingServiceImplTest.java
@@ -6,14 +6,14 @@ import at.fhv.hotelmanagement.application.dto.GuestDTO;
 import at.fhv.hotelmanagement.domain.infrastructure.HibernateBookingRepository;
 import at.fhv.hotelmanagement.domain.infrastructure.HibernateGuestRepository;
 import at.fhv.hotelmanagement.domain.model.*;
-import at.fhv.hotelmanagement.domain.model.enums.Country;
-import at.fhv.hotelmanagement.domain.model.enums.PaymentType;
-import at.fhv.hotelmanagement.domain.model.enums.Salutation;
+import at.fhv.hotelmanagement.domain.model.enums.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Profile("test")
 public class BookingServiceImplTest {
 
     @Autowired
@@ -37,44 +38,53 @@ public class BookingServiceImplTest {
     @Test
     void given_emptyrepository_when_fetchingallbookings_then_empty() {
         //given
-        Mockito.when(bookingRepository.findAll()).thenReturn(Collections.emptyList());
-
+        Mockito.when(this.bookingRepository.findAll()).thenReturn(Collections.emptyList());
         //when
-        List<BookingDTO> bookingsDto = bookingsService.allBookings();
-
+        List<BookingDTO> bookingsDto = this.bookingsService.allBookings();
         //then
         assertTrue(bookingsDto.isEmpty());
     }
 
     @Test
-    void given_2bookingsinrepository_when_fetchallbookings_then_returnequalbookings() {
+    void given_2bookingsinrepository_when_fetchallbookings_then_returnequalbookings() throws CreateGuestException, AlreadyExistsException {
         //given
+        Category category = CategoryFactory.createCategory("Business Casual EZ", "A casual accommodation for business guests.", 1);
+        category.createRoom(new Room(new RoomNumber("100"), RoomState.AVAILABLE));
+        category.createRoom(new Room(new RoomNumber("100"), RoomState.AVAILABLE));
+
         LocalDate arrivalDate = LocalDate.now();
         LocalDate departureDate = LocalDate.now().plusDays(2);
         LocalTime arrivalTime = LocalTime.of(11, 30);
-        Integer numberOfPersons = 4;
-        Map<String, Integer> selectCategoriesRoomCount = new HashMap<>();
-        selectCategoriesRoomCount.put("Honeymoon Suite DT", 2);
+        Integer numberOfPersons = 1;
+        Map<Category, Integer> selectCategoriesRoomCount = new HashMap<>();
+        selectCategoriesRoomCount.put(category, 1);
         GuestId guestId = new GuestId("1");
-        Address ad1 = new Address("Musterstrasse 1", "6850", "Dornbirn", String.valueOf(Country.AT));
-        Guest g = new Guest(guestId, null, String.valueOf(Salutation.MISTER), "Fritz", "Mayer", LocalDate.of(1979, 12, 24), ad1, "");
+        Address address = new Address("Musterstrasse 1", "6850", "Dornbirn", String.valueOf(Country.AT));
+        Guest guest = GuestFactory.createGuest(guestId, null, String.valueOf(Salutation.MISTER), "Fritz", "Mayer", LocalDate.of(1979, 12, 24), address, "");
         PaymentInformation paymentInformation = new PaymentInformation("Hans-Peter Mayer", "5432 9876 5678 1234", "12/21", "123", String.valueOf(PaymentType.INVOICE));
 
         Mockito.when(this.bookingRepository.nextIdentity()).thenReturn(new BookingNo(java.util.UUID.randomUUID().toString().toUpperCase()));
-
         BookingNo bookingNo1 = this.bookingRepository.nextIdentity();
         BookingNo bookingNo2 = this.bookingRepository.nextIdentity();
+
         List<BookingNo> bookingNos = Arrays.asList(bookingNo1, bookingNo2);
         List<Booking> bookings = bookingNos.stream()
-                .map(bookingNo -> new Booking(bookingNo, arrivalDate, departureDate, arrivalTime, numberOfPersons, selectCategoriesRoomCount, guestId, paymentInformation))
+                .map(bookingNo -> {
+                    try {
+                        return BookingFactory.createBooking(bookingNo, arrivalDate, departureDate, arrivalTime, numberOfPersons, selectCategoriesRoomCount, guestId, paymentInformation);
+                    } catch (CreateBookingException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
                 .collect(Collectors.toList());
 
         Mockito.when(this.bookingRepository.findAll()).thenReturn(bookings);
-        Mockito.when(this.guestRepository.findById(guestId)).thenReturn(Optional.of(g));
+        Mockito.when(this.guestRepository.findById(guestId)).thenReturn(Optional.of(guest));
 
         List<BookingDTO> bookingsDtoExpected = new ArrayList<>();
         for (Booking b : bookings) {
-            GuestDTO guestDto = GuestDTO.builder().withGuestEntity(g).build();
+            GuestDTO guestDto = GuestDTO.builder().withGuestEntity(guest).build();
             BookingDTO bookingDto = BookingDTO.builder().withBookingEntity(b).withDetails(BookingDetailsDTO.builder().withBookingEntity(b).withGuestDTO(guestDto).build()).build();
             bookingsDtoExpected.add(bookingDto);
         }
@@ -90,23 +100,26 @@ public class BookingServiceImplTest {
 
 
     @Test
-    void given_bookinginrepository_when_bybookingNo_then_return() {
+    void given_bookinginrepository_when_bybookingNo_then_return() throws CreateGuestException, CreateBookingException, AlreadyExistsException {
         //given
+        Category category = CategoryFactory.createCategory("Business Casual EZ", "A casual accommodation for business guests.", 1);
+        category.createRoom(new Room(new RoomNumber("100"), RoomState.AVAILABLE));
+
         LocalDate arrivalDate = LocalDate.now();
         LocalDate departureDate = LocalDate.now().plusDays(2);
         LocalTime arrivalTime = LocalTime.of(11, 30);
-        Integer numberOfPersons = 4;
-        Map<String, Integer> selectCategoriesRoomCount = new HashMap<>();
-        selectCategoriesRoomCount.put("Honeymoon Suite DT", 2);
+        Integer numberOfPersons = 1;
+        Map<Category, Integer> selectCategoriesRoomCount = new HashMap<>();
+        selectCategoriesRoomCount.put(category, 1);
         GuestId guestId = new GuestId("1");
         Address ad1 = new Address("Musterstrasse 1", "6850", "Dornbirn", String.valueOf(Country.AT));
-        Guest guest = new Guest(guestId, null, String.valueOf(Salutation.MISTER), "Fritz", "Mayer", LocalDate.of(1979, 12, 24), ad1, "");
+        Guest guest = GuestFactory.createGuest(guestId, null, String.valueOf(Salutation.MISTER), "Fritz", "Mayer", LocalDate.of(1979, 12, 24), ad1, "");
         PaymentInformation paymentInformation = new PaymentInformation("Hans-Peter Mayer", "5432 9876 5678 1234", "12/21", "123", String.valueOf(PaymentType.INVOICE));
 
         Mockito.when(this.bookingRepository.nextIdentity()).thenReturn(new BookingNo(java.util.UUID.randomUUID().toString().toUpperCase()));
 
         BookingNo bookingNo = this.bookingRepository.nextIdentity();
-        Booking booking = new Booking(bookingNo, arrivalDate, departureDate, arrivalTime, numberOfPersons, selectCategoriesRoomCount, guestId, paymentInformation);
+        Booking booking = BookingFactory.createBooking(bookingNo, arrivalDate, departureDate, arrivalTime, numberOfPersons, selectCategoriesRoomCount, guestId, paymentInformation);
 
         Mockito.when(this.guestRepository.findById(guestId)).thenReturn(Optional.of(guest));
 
@@ -130,23 +143,26 @@ public class BookingServiceImplTest {
 
 
     @Test
-    void given_bookingdetailsinrepository_when_bybookingNo_then_return() {
+    void given_bookingdetailsinrepository_when_bybookingNo_then_return() throws CreateGuestException, CreateBookingException, AlreadyExistsException {
         //given
+        Category category = CategoryFactory.createCategory("Business Casual EZ", "A casual accommodation for business guests.", 1);
+        category.createRoom(new Room(new RoomNumber("100"), RoomState.AVAILABLE));
+
         LocalDate arrivalDate = LocalDate.now();
         LocalDate departureDate = LocalDate.now().plusDays(2);
         LocalTime arrivalTime = LocalTime.of(11, 30);
-        Integer numberOfPersons = 4;
-        Map<String, Integer> selectCategoriesRoomCount = new HashMap<>();
-        selectCategoriesRoomCount.put("Honeymoon Suite DT", 2);
+        Integer numberOfPersons = 1;
+        Map<Category, Integer> selectCategoriesRoomCount = new HashMap<>();
+        selectCategoriesRoomCount.put(category, 1);
         GuestId guestId = new GuestId("1");
         Address ad1 = new Address("Musterstrasse 1", "6850", "Dornbirn", String.valueOf(Country.AT));
-        Guest guest = new Guest(guestId, null, String.valueOf(Salutation.MISTER), "Fritz", "Mayer", LocalDate.of(1979, 12, 24), ad1, "");
+        Guest guest = GuestFactory.createGuest(guestId, null, String.valueOf(Salutation.MISTER), "Fritz", "Mayer", LocalDate.of(1979, 12, 24), ad1, "");
         PaymentInformation paymentInformation = new PaymentInformation("Hans-Peter Mayer", "5432 9876 5678 1234", "12/21", "123", String.valueOf(PaymentType.INVOICE));
 
         Mockito.when(this.bookingRepository.nextIdentity()).thenReturn(new BookingNo(java.util.UUID.randomUUID().toString().toUpperCase()));
 
         BookingNo bookingNo = this.bookingRepository.nextIdentity();
-        Booking booking = new Booking(bookingNo, arrivalDate, departureDate, arrivalTime, numberOfPersons, selectCategoriesRoomCount, guestId, paymentInformation);
+        Booking booking = BookingFactory.createBooking(bookingNo, arrivalDate, departureDate, arrivalTime, numberOfPersons, selectCategoriesRoomCount, guestId, paymentInformation);
 
         Mockito.when(this.guestRepository.findById(guestId)).thenReturn(Optional.of(guest));
 
