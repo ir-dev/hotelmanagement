0871db2dbfbf6ebfa9b42e416e02d531845464cc
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2021-12-03 17:12:46 +0100

    AlreadyExistsException

--- src/main/java/at/fhv/hotelmanagement/domain/model/AlreadyExistsException.java
+++ src/main/java/at/fhv/hotelmanagement/domain/model/AlreadyExistsException.java
@@ -4,8 +4,4 @@ public class AlreadyExistsException extends Exception {
     public AlreadyExistsException (Class<?> clazz) {
         super(String.format("Item of type '%s' already exists", clazz.getSimpleName()));
     }
-
-    public AlreadyExistsException (Class<?> clazz, String duplicateName, String duplicateValue) {
-        super(String.format("Item of type '%s' already exists for '%s' with duplicate value '%s'", clazz.getSimpleName(), duplicateName, duplicateValue));
-    }
 }
